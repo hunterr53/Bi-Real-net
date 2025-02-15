557d546d57cb5bf685874092759aca31a83fceae
rwallac2 <rwallac2@uccs.edu>
2025-02-14 20:01:50 -0700

    Added debugging loader for first image.

--- pytorch_implementation/BiReal18_34/train.py
+++ pytorch_implementation/BiReal18_34/train.py
@@ -208,29 +208,44 @@ def main():
     # binImagesTest = binImagesTest.astype(np.uint8)
     # binImagesTest.tofile('pytorch_implementation/BiReal18_34/savedWeights/TransformedTestData.bin', sep='')
 
-    # Show first image after transform, and then save the first batch of images.
-    binImages = np.empty((164, 150529))
-    for (images, target) in val_loader: # For each batch
-        for i in range(args.batch_size):
-            label = target[i]
-            image = images[i]
-            # if i == 0:
-            #     print('Label: ' + str(label.data))
-            #     imagePrint = image.permute(1, 2, 0)
-            #     plt.imshow(imagePrint)
-            #     plt.show()
-            rowOfData = image.flatten().numpy()
-            rowOfData = np.insert(rowOfData, 0, label.data).reshape(1,-1) # Put label as first byte of data
-            binImages[i] = rowOfData
+    # # Show first image after transform, and then save the first batch of transformed images.
+    # binImages = np.empty((164, 150529))
+    # for (images, target) in val_loader: # For each batch
+    #     for i in range(args.batch_size):
+    #         label = target[i]
+    #         image = images[i]
+    #         # if i == 0:
+    #         #     print('Label: ' + str(label.data))
+    #         #     imagePrint = image.permute(1, 2, 0)
+    #         #     plt.imshow(imagePrint)
+    #         #     plt.show()
+    #         rowOfData = image.flatten().numpy()
+    #         rowOfData = np.insert(rowOfData, 0, label.data).reshape(1,-1) # Put label as first byte of data
+    #         binImages[i] = rowOfData
         
-        binImages = binImages.astype(np.float32) # 4 bytes per pixel
-        binImages.tofile('pytorch_implementation/BiReal18_34/savedWeights/TransformedTestData.bin', sep='')
+    #     binImages = binImages.astype(np.float32) # 4 bytes per pixel
+    #     binImages.tofile('pytorch_implementation/BiReal18_34/savedWeights/TransformedTestData.bin', sep='')
         
-        # test_df = pd.DataFrame(images[0][2].numpy().astype(np.float32))
-        # test_df.to_csv('pytorch_implementation/BiReal18_34/savedWeights/testImageZeroBlue.csv', index=False, header=False )
+    #     # test_df = pd.DataFrame(images[0][2].numpy().astype(np.float32))
+    #     # test_df.to_csv('pytorch_implementation/BiReal18_34/savedWeights/testImageZeroBlue.csv', index=False, header=False )
             
-        break # Only get first batch
+    #     break # Only get first batch
     # For a batch of 164, test file out should be: ((3x224x224) * 164images + 1 Label) * 4 bytes per pixel / 1024 BytesToKiloBytes
+
+    # Push First Test Image through model and save it to csv layer features
+    model.eval()
+    val_loader_debug = torch.utils.data.DataLoader(
+        val_dataset, batch_size=1, shuffle=False,
+        num_workers=1, pin_memory=True) # only one image per batch
+    with torch.no_grad():
+        for i, (images, target) in enumerate(val_loader_debug):
+            images = images.cuda() if isCuda else images.cpu()
+            target = target.cuda() if isCuda else target.cpu()
+            # compute output
+            logits = model(images)
+            loss = criterion(logits, target)
+
+            break # Only get first batch/image
 
     # train the model
     epoch = start_epoch
